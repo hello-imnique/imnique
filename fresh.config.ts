b8bce3f83a90753abf56badbc7139da7e527bf10
Nik Kita <nik-kita@on.github>
2024-12-25 07:59:23 +0100

    specify port
    the port is registered on github for local dev purposes - that's why it
    is hardcoded
    
    (on deno-deploy it is ignored)

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
+import { defineConfig } from "$fresh/server.ts";
 import kv_oauth from "./plugins/kv_oauth/mod.ts";
 
 export default defineConfig({
+  server: {
+    port: 4000,
+  },
   plugins: [tailwind(), kv_oauth],
 });
